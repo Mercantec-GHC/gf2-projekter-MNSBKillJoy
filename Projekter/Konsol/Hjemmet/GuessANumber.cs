920edf061ee702301d29843bbf5132d5d149b66d
MariaNSB <mariabeuc@gmail.com>
2025-09-10 14:49:50 +0200

    UpdateDay10-09

--- Projekter/Konsol/Hjemmet/GuessANumber.cs
+++ Projekter/Konsol/Hjemmet/GuessANumber.cs
@@ -4,7 +4,13 @@ namespace Hjemmet
     {
         public void Start()
         {
-            Console.WriteLine("Gæt et tal er ikke implementeret endnu.");
+            Console.WriteLine("Guess a number between 1 and 100000");
+
+            Random brains = new Random();
+            int brainsnum = brains.Next(1, 100000);
+
+            Console.WriteLine($"The brain may be thinking of {brainsum}");
+
             Console.ReadKey();
         }
     }
